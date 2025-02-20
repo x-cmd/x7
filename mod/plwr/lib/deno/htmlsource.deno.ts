128945474caa3c4c0066412c7a265c529b10cbf3
l <l@x-cmd.com>
2025-02-20 11:57:26 +0000

    ...

--- mod/plwr/lib/deno/htmlsource.deno.ts
+++ mod/plwr/lib/deno/htmlsource.deno.ts
@@ -3,9 +3,14 @@ import { chromium } from 'npm:playwright'
 const url = Deno.args?.[0] || 'https://mistral.ai/en/news'
 const state = Deno.args?.[1] || 'networkidle'
 
+const proxy = Deno.env.get('HTTP_PROXY') || Deno.env.get('http_proxy')
+
 ;(async function () {
     const browser = await chromium.launch({
         headless: true,
+        proxy: proxy ? {
+            server: proxy,
+        } : undefined,
     })
     const page = await browser.newPage()
     await page.goto(url)
